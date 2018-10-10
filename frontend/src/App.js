7d565ea5a75e85329e6f77105e209474ccbf3a2d
DirectX <DirectX@users.noreply.github.com>
2018-10-10 19:36:00 +0300

    Addded FontAwesome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import { Alert, Button, Container, Form, FormGroup, CustomInput, Nav, NavItem, NavLink, Row, Col } from 'reactstrap';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDownload, faShareAlt, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { Alert, Button, Container, Form, FormGroup, CustomInput, Nav, NavItem, NavLink, Row, Col, Modal, ModalHeader, ModalBody, ModalFooter, Label, Input } from 'reactstrap';
 import Web3 from 'web3';
 import FileSaver from 'file-saver';
 import { sha256Hash } from './sha256';
 import './App.css';
+
+library.add(faDownload, faShareAlt, faTrash);
 
 function humanFileSize(bytes, si) {
   var thresh = si ? 1000 : 1024;
@@ -49,8 +54,15 @@ class App extends Component {
 
     this.onSelectFiles = this.onSelectFiles.bind(this);
     this.onUpload = this.onUpload.bind(this);
-    this.onCancel = this.onCancel.bind(this);
+    this.onUploadCancel = this.onUploadCancel.bind(this);
     this.onDownload = this.onDownload.bind(this);
+    this.toggleShareModal = this.toggleShareModal.bind(this);
+    this.openShareModal = this.openShareModal.bind(this);
+    this.handleAddressChange = this.handleAddressChange.bind(this);
+    this.onShareSubmit = this.onShareSubmit.bind(this);
+    this.toggleDeleteModal = this.toggleDeleteModal.bind(this);
+    this.openDeleteModal = this.openDeleteModal.bind(this);
+    this.onDeleteSubmit = this.onDeleteSubmit.bind(this);
 
     this.state.web3js.eth.net.getNetworkType().then((networkName) => {
       if (networkName !== 'rinkeby') {
@@ -184,7 +196,7 @@ class App extends Component {
     }
   }
 
-  async onCancel(event) {
+  async onUploadCancel(event) {
     if (this.state.result.length > 0)
       this.loadUserFiles();
 
@@ -211,6 +223,103 @@ class App extends Component {
       alert('Unathorized');
     }
   }
+
+   toggleShareModal(hash) {
+    this.setState({ shareModalOpen: !this.state.shareModalOpen });
+  }
+
+  openShareModal(hash) {
+    this.setState({
+      shareModalOpen: true,
+      shareHash: hash,
+      shareAddress: '',
+    });
+  }
+
+  handleAddressChange(event) {
+    this.setState({shareAddress: event.target.value});
+  }
+
+  async onShareSubmit(event) {
+    event.preventDefault();
+
+    try {
+    } catch(err) {
+      console.log('Error:', err);
+    };
+  }
+
+  toggleDeleteModal(hash) {
+    this.setState({ deleteModalOpen: !this.state.deleteModalOpen });
+  }
+
+  openDeleteModal(hash) {
+    this.setState({
+      deleteModalOpen: true,
+      deleteHash: hash,
+    });
+  }
+
+  async onDeleteSubmit(event) {
+    event.preventDefault();
+
+    const self = this;
+    const hash = this.state.deleteHash;
+    const url = `${process.env.REACT_APP_API_URL_PREFIX}/${hash}`;
+
+    try {
+      const response = await fetch(url, {
+        method: 'DELETE',
+      });
+    } 
+    catch (err) {  
+      console.log('Request error:', err);
+    }
+  }
+
+  // async onShare(hash) {
+  //   try {
+  //     console.log('Share');
+  //     // const dataHash = this.state.web3js.utils.sha3(hash);
+  //     // const signature = await this.state.web3js.eth.personal.sign(dataHash, this.state.account);
+
+  //     // let data = new FormData()
+  //     // data.append('meta', JSON.stringify({ owner: this.state.account, signature: signature }));
+
+  //     // let response = await fetch(`${process.env.REACT_APP_API_URL_PREFIX}/download/${hash}`, {
+  //     //   method: 'POST',
+  //     //   body: data,
+  //     // });
+
+  //     // let blob = await response.blob();
+
+  //     // FileSaver.saveAs(blob, filename);
+  //   } catch (err) {
+  //     alert('Unathorized');
+  //   }
+  // }
+
+  // async onDelete(hash) {
+  //   try {
+  //     console.log('Remove');
+  //     // const dataHash = this.state.web3js.utils.sha3(hash);
+  //     // const signature = await this.state.web3js.eth.personal.sign(dataHash, this.state.account);
+
+  //     // let data = new FormData()
+  //     // data.append('meta', JSON.stringify({ owner: this.state.account, signature: signature }));
+
+  //     // let response = await fetch(`${process.env.REACT_APP_API_URL_PREFIX}/download/${hash}`, {
+  //     //   method: 'POST',
+  //     //   body: data,
+  //     // });
+
+  //     // let blob = await response.blob();
+
+  //     // FileSaver.saveAs(blob, filename);
+  //   } catch (err) {
+  //     alert('Unathorized');
+  //   }
+  // }
 
   render() {
     return (
@@ -289,12 +398,12 @@ class App extends Component {
             <br />
             <div className="text-center">
               <Button className="mr-2" color="success" size="lg" onClick={this.onUpload}>Sign and Upload&hellip;</Button>
-              <Button color="secondary" size="lg" onClick={this.onCancel}>Cancel</Button>
+              <Button color="secondary" size="lg" onClick={this.onUploadCancel}>Cancel</Button>
             </div>
           </div>
           <div hidden={!this.state.uploaded || this.state.newFiles.length === 0} className="files-panel">
             <div className="text-center">
-              <Button color="primary" size="lg" onClick={this.onCancel}>Done</Button>
+              <Button color="primary" size="lg" onClick={this.onUploadCancel}>Done</Button>
             </div>
           </div>
           <div className="files-panel">
@@ -304,11 +413,44 @@ class App extends Component {
                 <Row className="lead align-items-center">
                   <Col className="col-8 text-truncate" title={file.filename}><span className="font-weight-bold">{file.filename}</span><br />{file.hash}</Col>
                   <Col className="col-2 text-right">{humanFileSize(file.contentSize, true)}</Col>
-                  <Col className="col-2 text-right"><Button color="primary" outline onClick={() => this.onDownload(file.hash, file.filename)}>Download...</Button></Col>
+                  <Col className="col-2 text-right">
+                    <Button color="success" onClick={() => this.onDownload(file.hash, file.filename)} title="Download"><FontAwesomeIcon className="fa-fw" icon="download" /></Button>
+                    <Button className="ml-2" color="primary" onClick={() => this.openShareModal(file.hash)} title="Share"><FontAwesomeIcon className="fa-fw" icon="share-alt" /></Button>
+                    <Button className="ml-2" color="danger" onClick={() => this.openDeleteModal(file.hash)} title="Delete"><FontAwesomeIcon className="fa-fw" icon="trash" /></Button>
+                  </Col>
                 </Row>
               </Container>
             }, this)}
           </div>
+          <Modal isOpen={this.state.shareModalOpen} toggle={this.toggleShareModal}>
+            <Form onSubmit={this.onShareSubmit}>
+              <ModalHeader toggle={this.toggleShareModal}>Share File</ModalHeader>
+              <ModalBody>
+                <FormGroup row>
+                  <Label for="addressTo" sm={2}>Address</Label>
+                  <Col sm={10}>
+                    <Input type="text" name="addressTo" id="addressTo" placeholder="0x0000000000000000000000000000000000000000" value={this.state.shareAddress} onChange={this.handleAddressChange} />
+                  </Col>
+                </FormGroup>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" type="submit" className="mr-2"><FontAwesomeIcon className="fa-fw" icon="share-alt" /> <span className="d-none d-sm-inline">Share</span></Button>
+                <Button color="secondary" onClick={this.toggleShareModal}>Cancel</Button>
+              </ModalFooter>
+            </Form>
+          </Modal>
+          <Modal isOpen={this.state.deleteModalOpen} toggle={this.toggleDeleteModal}>
+            <Form onSubmit={this.onDeleteSubmit}>
+              <ModalHeader toggle={this.toggleDeleteModal}>Delete File</ModalHeader>
+              <ModalBody>
+                <p className="lead">This action is non-reversible</p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" type="submit" className="mr-2"><FontAwesomeIcon className="fa-fw" icon="trash" /> <span className="d-none d-sm-inline">Delete</span></Button>
+                <Button color="secondary" onClick={this.toggleDeleteModal}>Cancel</Button>
+              </ModalFooter>
+            </Form>
+          </Modal>
         </Container>
       </div>
     );
